eae45b4f8d5e6ef5a53407493fa7fd49e4c22c04
Richard Harrah <topplethenunnery@gmail.com>
2015-05-18 16:25:54 -0400

    initial commit

---
+++ desktop/src/com/tealcube/java/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tealcube.java.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tealcube.java.games.TacirGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TacirGame(), config);
+	}
+}
