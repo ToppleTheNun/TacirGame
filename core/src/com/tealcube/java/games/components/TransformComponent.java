794641a9b919ee2403b46ed2e2f0eb12943148fd
Richard Harrah <topplethenunnery@gmail.com>
2015-05-20 01:16:48 -0400

    TransformComponent supports 3D

--- core/src/com/tealcube/java/games/components/TransformComponent.java
+++ core/src/com/tealcube/java/games/components/TransformComponent.java
@@ -1,35 +1,35 @@
 package com.tealcube.java.games.components;
 
 import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
 public class TransformComponent extends Component implements Poolable {
 
-    private Vector2 position = Vector2.Zero;
-    private Vector2 scale = new Vector2(1.0f, 1.0f);
+    private Vector3 position = Vector3.Zero;
+    private Vector3 scale = new Vector3(1.0f, 1.0f, 1.0f);
     private float rotation = 0.0f;
 
     @Override
     public void reset() {
-        position = Vector2.Zero;
-        scale = new Vector2(1.0f, 1.0f);
+        position = Vector3.Zero;
+        scale = new Vector3(1.0f, 1.0f, 1.0f);
         rotation = 0.0f;
     }
 
-    public Vector2 getPosition() {
+    public Vector3 getPosition() {
         return position;
     }
 
-    public void setPosition(Vector2 position) {
+    public void setPosition(Vector3 position) {
         this.position = position;
     }
 
-    public Vector2 getScale() {
+    public Vector3 getScale() {
         return scale;
     }
 
-    public void setScale(Vector2 scale) {
+    public void setScale(Vector3 scale) {
         this.scale = scale;
     }
 
