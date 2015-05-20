a5f637fb597ec18746dc15c7e1b4629780399431
Richard Harrah <topplethenunnery@gmail.com>
2015-05-19 23:44:10 -0400

    adding valid ID check

--- core/src/com/tealcube/java/games/systems/MovementSystem.java
+++ core/src/com/tealcube/java/games/systems/MovementSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.tealcube.java.games.Mappers;
+import com.tealcube.java.games.TacirGame;
 import com.tealcube.java.games.components.PositionComponent;
 import com.tealcube.java.games.components.VelocityComponent;
 
@@ -15,6 +16,11 @@ public class MovementSystem extends IteratingSystem {
 
     @Override
     public void processEntity(Entity entity, float deltaTime) {
+        // check if the entity is valid
+        if (entity.getId() == TacirGame.INVALID_ENTITY_ID) {
+            return;
+        }
+
         // below is faster than "entity.getComponent(PositionComponent.class)" or whatever
         // the entity equivalent is, but the source code is dumb
         PositionComponent position = Mappers.getInstance().getPositionMapper().get(entity);
