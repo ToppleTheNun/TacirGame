c50cf3eda6e7b3d214369b7e0e53ccc8b9a48913
Richard Harrah <topplethenunnery@gmail.com>
2015-05-20 00:39:33 -0400

    rearranging order of update

--- core/src/com/tealcube/java/games/TacirGame.java
+++ core/src/com/tealcube/java/games/TacirGame.java
@@ -63,11 +63,10 @@ public class TacirGame extends ApplicationAdapter {
 
     @Override
     public void render() {
-        // update the game BEFORE rendering
-        engine.update(Gdx.graphics.getDeltaTime());
-
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        engine.update(Gdx.graphics.getDeltaTime());
     }
 
     @Override
