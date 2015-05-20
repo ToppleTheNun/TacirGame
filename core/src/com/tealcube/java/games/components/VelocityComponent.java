4d7e90e61eaa09ead59f6031747c6f6344aae348
Richard Harrah <topplethenunnery@gmail.com>
2015-05-20 01:18:17 -0400

    updating VelocityComponent to use 3D

--- core/src/com/tealcube/java/games/components/VelocityComponent.java
+++ core/src/com/tealcube/java/games/components/VelocityComponent.java
@@ -1,23 +1,23 @@
 package com.tealcube.java.games.components;
 
 import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
 public class VelocityComponent extends Component implements Poolable {
 
-    private Vector2 velocity = Vector2.Zero;
+    private Vector3 velocity = Vector3.Zero;
 
     @Override
     public void reset() {
-        velocity = Vector2.Zero;
+        velocity = Vector3.Zero;
     }
 
-    public Vector2 getVelocity() {
+    public Vector3 getVelocity() {
         return velocity;
     }
 
-    public void setVelocity(Vector2 velocity) {
+    public void setVelocity(Vector3 velocity) {
         this.velocity = velocity;
     }
 }
