5f429839a8f7277e53b91d8aa7083a2dd23cc5cc
Richard Harrah <topplethenunnery@gmail.com>
2015-05-19 23:38:36 -0400

    registering movement system

--- core/src/com/tealcube/java/games/TacirGame.java
+++ core/src/com/tealcube/java/games/TacirGame.java
@@ -1,27 +1,57 @@
 package com.tealcube.java.games;
 
+import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.tealcube.java.games.systems.MovementSystem;
 
 public class TacirGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    // The following four constants define the amount of entities
+    // and components that we are able to have in our game
+    public static final int ENTITY_POOL_INITIAL_SIZE = 10;
+    public static final int ENTITY_POOL_MAX_SIZE = 100;
+    public static final int COMPONENT_POOL_INITIAL_SIZE = 10;
+    public static final int COMPONENT_POOL_MAX_SIZE = 100;
+
+    // In the pooled entity system, a valid entity will always
+    // have an ID that is NOT equal to 0
+    public static final long INVALID_ENTITY_ID = 0L;
+
+    // Entity/Component System engine, tracks all entities
+    private PooledEngine engine;
+
+    // EntitySystems that want to be tracked
+    private MovementSystem movementSystem;
+
+    @Override
+    public void create() {
+        // initialize the ECS engine
+        engine = new PooledEngine(ENTITY_POOL_INITIAL_SIZE, ENTITY_POOL_MAX_SIZE, COMPONENT_POOL_INITIAL_SIZE,
+                                  COMPONENT_POOL_MAX_SIZE);
+
+        // register the Movement system
+        movementSystem = new MovementSystem();
+        engine.addSystem(movementSystem);
+    }
+
+    @Override
+    public void render() {
+        // update the game BEFORE rendering
+        engine.update(Gdx.graphics.getDeltaTime());
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    public PooledEngine getEngine() {
+        return engine;
+    }
+
 }
