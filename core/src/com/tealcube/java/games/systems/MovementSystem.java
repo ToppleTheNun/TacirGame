fbfcf06afc33a66b83a7f63a5060212d4f26fa11
Richard Harrah <topplethenunnery@gmail.com>
2015-05-19 23:29:45 -0400

    adding MovementSystem

---
+++ core/src/com/tealcube/java/games/systems/MovementSystem.java
@@ -0,0 +1,27 @@
+package com.tealcube.java.games.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.tealcube.java.games.Mappers;
+import com.tealcube.java.games.components.PositionComponent;
+import com.tealcube.java.games.components.VelocityComponent;
+
+public class MovementSystem extends IteratingSystem {
+
+    public MovementSystem() {
+        super(Family.all(PositionComponent.class, VelocityComponent.class).get());
+    }
+
+    @Override
+    public void processEntity(Entity entity, float deltaTime) {
+        // below is faster than "entity.getComponent(PositionComponent.class)" or whatever
+        // the entity equivalent is, but the source code is dumb
+        PositionComponent position = Mappers.getInstance().getPositionMapper().get(entity);
+        VelocityComponent velocity = Mappers.getInstance().getVelocityMapper().get(entity);
+
+        position.setX(position.getX() + velocity.getX() * deltaTime);
+        position.setY(position.getY() + velocity.getY() * deltaTime);
+    }
+
+}
