1aacf9eea52073e36d48ec1ad6aaae7519b4c009
Richard Harrah <topplethenunnery@gmail.com>
2015-05-21 02:59:25 -0400

    removing classes that are now in tealcube-commons

--- core/src/com/tealcube/java/games/common/events/Cancellable.java
+++
@@ -1,9 +0,0 @@
-package com.tealcube.java.games.common.events;
-
-public interface Cancellable {
-
-    boolean isCancelled();
-
-    void setCancelled(boolean value);
-
-}
--- core/src/com/tealcube/java/games/common/events/EventExecutor.java
+++
@@ -1,7 +0,0 @@
-package com.tealcube.java.games.common.events;
-
-public interface EventExecutor {
-
-    void execute(Listener listener, Event event) throws EventException;
-
-}
