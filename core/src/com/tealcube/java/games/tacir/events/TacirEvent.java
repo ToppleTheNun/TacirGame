b80b8e6952c4a57a29da9894bfd32b13df39b181
Richard Harrah <topplethenunnery@gmail.com>
2015-05-21 01:29:51 -0400

    adding TacirEvent

---
+++ core/src/com/tealcube/java/games/tacir/events/TacirEvent.java
@@ -0,0 +1,19 @@
+package com.tealcube.java.games.tacir.events;
+
+import com.tealcube.java.games.common.events.Event;
+import com.tealcube.java.games.common.events.HandlerList;
+
+public class TacirEvent extends Event {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+}
