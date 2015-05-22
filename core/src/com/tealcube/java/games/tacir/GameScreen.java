6757e9db1ec41bff92d1a569723928c95f222517
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2015-05-22 14:09:08 -0400

    Adding a few things to screens

--- core/src/com/tealcube/java/games/tacir/GameScreen.java
+++ core/src/com/tealcube/java/games/tacir/GameScreen.java
@@ -1,8 +1,49 @@
 package com.tealcube.java.games.tacir;
 
-/**
- * Created by Kyle on 5/22/2015.
- */
-public class GameScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
+public class GameScreen extends ScreenAdapter {
+
+    TacirGame game;
+    OrthographicCamera guiCam;
+    Rectangle muteButton;
+    Rectangle pauseButton;
+    Vector3 touchPoint;
+
+    public GameScreen(TacirGame game) {
+        this.game = game;
+
+        guiCam = new OrthographicCamera(800, 450);
+        guiCam.position.set(800 / 2, 480 / 2, 0);
+        muteButton = new Rectangle(0, 0, 64, 64);
+        pauseButton = new Rectangle(160 - 150, 200 + 18, 300, 36);
+        touchPoint = new Vector3();
+    }
+
+    public void update() {
+        if (Gdx.input.justTouched()) {
+            guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+            if (muteButton.contains(touchPoint.x, touchPoint.y)) {
+                // Mute Game
+            }
+            if (pauseButton.contains(touchPoint.x, touchPoint.y)) {
+                // Pause Game
+            }
+        }
+    }
+
+    public void draw() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        update();
+        draw();
+    }
 }
--- core/src/com/tealcube/java/games/tacir/MenuScreen.java
+++ core/src/com/tealcube/java/games/tacir/MenuScreen.java
@@ -1,8 +1,59 @@
 package com.tealcube.java.games.tacir;
 
-/**
- * Created by Kyle on 5/22/2015.
- */
-public class MenuScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
+public class MenuScreen  extends ScreenAdapter {
+
+    TacirGame game;
+    OrthographicCamera guiCam;
+    Rectangle playButton;
+    Rectangle optionsButton;
+    Rectangle unlocksButton;
+    Rectangle exitButton;
+    Vector3 touchPoint;
+
+    public MenuScreen(TacirGame game) {
+        this.game = game;
+
+        guiCam = new OrthographicCamera(800, 450);
+        guiCam.position.set(800 / 2, 480 / 2, 0);
+        playButton = new Rectangle(0, 0, 64, 64);
+        optionsButton = new Rectangle(160 - 150, 200 + 18, 300, 36);
+        unlocksButton = new Rectangle(160 - 150, 200 - 18, 300, 36);
+        exitButton = new Rectangle(160 - 150, 200 - 18 - 36, 300, 36);
+        touchPoint = new Vector3();
+    }
+
+    public void update() {
+        if (Gdx.input.justTouched()) {
+            guiCam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+
+            if (playButton.contains(touchPoint.x, touchPoint.y)) {
+
+            }
+            if (unlocksButton.contains(touchPoint.x, touchPoint.y)) {
+
+            }
+            if (optionsButton.contains(touchPoint.x, touchPoint.y)) {
+
+            }
+            if (exitButton.contains(touchPoint.x, touchPoint.y)) {
+
+            }
+        }
+    }
+
+    public void draw() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        update();
+        draw();
+    }
 }
